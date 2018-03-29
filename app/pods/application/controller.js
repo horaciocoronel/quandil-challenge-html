436ce670f5aea0142b25de28f8b8a0932925a2bd
horaciocoronel <arielc@gmail.com>
2018-03-29 14:40:43 -0400

    Add function in application/controller to filter navbar when in certain routes

---
+++ app/pods/application/controller.js
@@ -0,0 +1,7 @@
+// Exports a function that will return true if the currentRouteName is not present in the hideNavRoutes array.
+export default Ember.Controller.extend({
+   hideNavRoutes: ['products.show'],
+    isOnHideNavRoutes: Ember.computed('currentRouteName', function(){
+      return this.get('hideNavRoutes').indexOf(this.get('currentRouteName')) !== -1;
+    })
+});
