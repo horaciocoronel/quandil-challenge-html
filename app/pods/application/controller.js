e72abe745a05ee94831e51631b697a6411863c37
horaciocoronel <arielc@gmail.com>
2018-03-29 18:13:31 -0400

    Add click action to printout a message in the console

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,7 +1,12 @@
 // Exports a function that will return true if the currentRouteName is not present in the hideNavRoutes array.
 export default Ember.Controller.extend({
-   hideNavRoutes: ['products.show'],
-    isOnHideNavRoutes: Ember.computed('currentRouteName', function(){
-      return this.get('hideNavRoutes').indexOf(this.get('currentRouteName')) !== -1;
-    })
+	hideNavRoutes: ['products.show'],
+	isOnHideNavRoutes: Ember.computed('currentRouteName', function(){
+		return this.get('hideNavRoutes').indexOf(this.get('currentRouteName')) !== -1;
+	  }),
+	actions: {
+		click(title) {
+			console.log('I was clicked');
+		}
+	}
 });
